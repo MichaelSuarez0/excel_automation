247335a54d5060895c5f5b7266e700fc0b778277
MichaelSuarez0 <a20180264@pucp.edu.pe>
2025-02-10 00:02:35 -0500

    Update excel_formatter.py
    - ExcelFormatter class now in charge of writing to excel and applying format.

--- classes/core/excel_formatter.py
+++ classes/core/excel_formatter.py
@@ -1,180 +1,135 @@
-import openpyxl
-from openpyxl import Workbook
-from openpyxl.styles import Alignment, PatternFill, Font, Border, Side
-from openpyxl.styles.numbers import FORMAT_DATE_XLSX17
-import datetime
-from excel_automation.classes.formats.colors import Color
 import os
+import pandas as pd
+from xlsxwriter.workbook import Workbook
+from xlsxwriter.worksheet import Worksheet
+from excel_automation.classes.formats.colors import Color
+from excel_automation.classes.formats.formats import Formats
+from typing import Tuple, Literal
+import numpy as np
 
 script_dir = os.path.abspath(os.path.dirname(__file__))
-#macros_folder = os.path.join(script_dir, "..", "macros", "excel")
-save_dir = os.path.join(script_dir, "..", "charts")
+save_dir = os.path.join(script_dir, "..", "..", "charts")
+
 
 class ExcelFormatter:
-    def __init__(self, file_name: str = "", workbook: Workbook = None):
-        """Class to open Excel files and apply beautiful format. 
-        Engine: openpyxl
+    def __init__(self, df_list: list[pd.DataFrame], output_name: str = "ExcelAutoChart"):
+        """Class to write to Excel files from DataFrames and applying format. Engine: xlsxwriter
 
         Parameters
         ----------
-        workbook : Workbook
-            The workbook to be formatted.
+        df_list : list(pd.DataFrame):
+            Data that will be written to Excel
+        output_name : str, optional: 
+            File name for the output file. Defaults to "ExcelAutoChart".
         """
-        self.file_path = os.path.join(script_dir, "..", "charts", f'{file_name}.xlsx')
-        self.wb = openpyxl.load_workbook(self.file_path, data_only=True) if file_name else workbook
-        # Access different worksheets with self.wb.sheetnames[int]
-        self.ws = self.wb.active
-
-    def _get_worksheet_position(self, position: str | int) -> int:
-        if position == "start":
-            index = 0 
-        elif position == "end":
-            index = len(self.wb.sheetnames)
-        elif isinstance(position, int):
-            if position < 0 or position > len(self.wb.sheetnames):
-                raise ValueError(f"Invalid index {position}. Index must be between 0 and {len(self.wb.sheetnames)}.")
-            index = position  # Add at the specified index
-        else:
-            raise ValueError(f"Invalid position '{position}'. Use 'start', 'end', or an integer index.")
-        return index
-
-    def get_worksheet_by_index(self, index: int):
-        """Returns the worksheet at the specified index.
-        Wrapper around self.wb.worsheets[index].
-
-        Parameters
-        ----------
-        index : int
-            The index of the worksheet to be accessed.
-
-        Returns
-        -------
-        worksheet : Worksheet
-            The worksheet at the specified index.
-        """
-        if 0 <= index < len(self.wb.worksheets):
-            self.ws = self.wb.worksheets[index]
-            return self.ws
-        else:
-            raise IndexError(f"Worksheet index {index} is out of range. The workbook has {len(self.wb.worksheets)} worksheets.")
+        self.output_name = output_name
+        self.writer = pd.ExcelWriter(os.path.join(save_dir, f'{output_name}.xlsx'), engine='xlsxwriter')
+        self.workbook: Workbook = self.writer.book
+        self.df_list = df_list
+        self.sheet_dfs = {}
+        self.format = Formats()
+    
+    def _write_to_excel(self, df: pd.DataFrame, sheet_name: str, num_format: str, format: Literal["database", "index"] | None = "database") -> Tuple[pd.DataFrame, Worksheet]:
+        df.to_excel(self.writer, sheet_name=sheet_name, index=False)
+        worksheet = self.writer.sheets[sheet_name]
+        self.sheet_dfs[sheet_name] = df
+        if format == "database":
+            self.apply_database_format(worksheet, df, num_format)
+        if format == "index":
+            self.apply_index_format(worksheet, df, num_format)
         
-    def create_new_worksheet(self, sheet_name: str = "Index", position: str | int = "end") -> None:
-        """
-        Creates a new worksheet in the workbook and adds it to the specified position.
+        return df, worksheet
 
-        Parameters
-        ----------
-        sheet_name : str
-            The name of the new worksheet to create. Defaults to "Index".
-        position : str | int, optional
-            The position where the new worksheet should be added. Options:
-            - "start": Adds the worksheet as the first sheet.
-            - "end": Adds the worksheet as the last sheet (default).
-            - int: Adds the worksheet at the specified index (0-based).
-
-        Returns
-        -------
-        None
-        """
-        if sheet_name in self.wb.sheetnames:
-            raise ValueError(f"A worksheet with the name '{sheet_name}' already exists.")
-
-        index = self._get_worksheet_position(position)
-
-        # Create the new worksheet at the specified index
-        self.wb.create_sheet(title=sheet_name, index=index)
-        print(f"✅ Worksheet '{sheet_name}' created at position {index}.")
+    def save_workbook(self):
+        self.writer.close()
+        print(f'✅ Excel guardado como "{self.output_name}"')
         
 
-    def apply_database_format(self, position: str | int)-> None:
-        """
-        Applies a standardized database-like format to the current worksheet to improve visual appeal:
+    def apply_database_format(self, worksheet: Worksheet, df: pd.DataFrame, num_format: str):
+        """Applies formatting only to cells with data."""
 
-        - Sets column widths and hides gridlines.
-        - Applies number formatting to numeric cells (with optional decimal places).
-        - Formats the first column (assumed to contain years or dates) with a light gray background.
-        - Formats the first row (assumed to contain headers) with a dark blue background, bold white text, and centered alignment.
-        - Applies light border to data containing cells.
+        # Set column widths
+        worksheet.set_column('A:A', 15)
+        if len(df.columns) > 1:
+            if len(str(df.iloc[0,1])) > 11:
+                worksheet.set_column(1, len(df.columns) - 1, 14)
+            else:
+                worksheet.set_column(1, len(df.columns) - 1, 10)
 
-        Parameters
-        ----------
-        position : str | int, optional
-            The position where the new worksheet should be added. Options:
-            - "start": Adds the worksheet as the first sheet.
-            - "end": Adds the worksheet as the last sheet (default).
-            - int: Adds the worksheet at the specified index (0-based).
+        # Hide gridlines
+        worksheet.hide_gridlines(2)
 
-        Returns
-        -------
-        None
-        """
-        index = self._get_worksheet_position(position)
-        self.get_worksheet_by_index(index) # Updates self.ws
+        # Determine format for the first column and adjust for datetime
+        first_col = df.columns[0]
+        first_col_fmt = self.workbook.add_format(self.format.cells['first_column'])
+        if pd.api.types.is_datetime64_any_dtype(df[first_col]):
+            first_col_fmt.set_num_format('mmm-yy')
 
-        self.ws.column_dimensions['A'].width = 15
-        self.ws.sheet_view.showGridLines = False
-        w = Color.WHITE.no_hash
-        g = Color.GRAY_LIGHT.no_hash
-        b = Color.BLUE_DARK.no_hash
+        # Write headers with header format
+        for col_num, col_name in enumerate(df.columns):
+            worksheet.write(0, col_num, col_name, self.workbook.add_format(self.format.cells["header"]))
 
-        if not isinstance(self.ws['B2'].value, (int, float)):
-            print(f'La celda B2 de la hoja {index+1}  no contiene un valor de tipo número. Verifica el formato de las celdas')
-        decimals = isinstance(self.ws['B2'].value, float)
+        # Define format for numeric data columns once
+        fmt = self.workbook.add_format(self.format.cells['data'])
+        fmt.set_num_format(num_format)
 
-        # Data iteration: number formatting to cells containing data
-        vanilla_border = Border(left=Side(style='thin', color=g), right=Side(style='thin', color=g), top=Side(style='thin', color=g), bottom=Side(style='thin', color=g))
-        for row in self.ws.iter_rows(min_row=2, max_row=self.ws.max_row, min_col=2, max_col=self.ws.max_column):
-            for cell in row:
-                if isinstance(cell.value, (int, float)):
-                    cell.number_format = "0.00" if decimals else "0"
-                    cell.border = vanilla_border
+        # Write data cells with appropriate formats
+        for row_idx in range(df.shape[0]):
+            # First column (e.g., dates or text)
+            cell_value = df.iloc[row_idx, 0]
+            worksheet.write(row_idx + 1, 0, cell_value, first_col_fmt)
 
-        # First column (years)
-        fill = PatternFill(start_color=g, fill_type="solid")
-        custom_border = Border(left=Side(style='thin', color=w), right=Side(style='thin', color=w), top=Side(style='thin', color=w), bottom=Side(style='thin', color=w))
-        for row in self.ws.iter_rows(min_row=1, max_row=self.ws.max_row, min_col=1, max_col=1):
-            for cell in row:
-                cell.fill = fill
-                cell.border = custom_border
-                # Check if the cell contains a datetime value
-                if isinstance(cell.value, (datetime.date, datetime.datetime)):
-                    cell.number_format = FORMAT_DATE_XLSX17  # Apply mmm-yy format
+            # Other columns (numeric data)
+            for col_idx in range(1, df.shape[1]):
+                cell_value = df.iloc[row_idx, col_idx]
 
-        # First row (categories)
-        fill = PatternFill(start_color=b, fill_type="solid")
-        for row in self.ws.iter_rows(min_row=1, max_row=1, min_col=1, max_col=self.ws.max_column):
-            for cell in row:
-                cell.fill = fill
-                cell.font = Font(color=w, bold=True)
-                cell.border = custom_border
-                cell.alignment = Alignment(horizontal= "center", vertical="center")  # Ajustar texto. TODO: no funciona en excel en línea
+                # Skip NaN/Inf values by checking if the value is NaN or Inf
+                if pd.isna(cell_value) or (isinstance(cell_value, (int, float)) and np.isinf(cell_value)):
+                    worksheet.write(row_idx + 1, col_idx, '')  # Write an empty cell
+                else:
+                    worksheet.write(row_idx + 1, col_idx, cell_value, fmt)
 
+    def apply_index_format(self, worksheet: Worksheet, df: pd.DataFrame, num_format: str):
+        """NOT IMPLEMENTED"""
+        # Set column widths
+        worksheet.set_column('A:A', 15)
+        if len(df.columns) > 1:
+            if len(str(df.iloc[0,1])) > 11:
+                worksheet.set_column(1, len(df.columns) - 1, 14)
+            else:
+                worksheet.set_column(1, len(df.columns) - 1, 10)
 
-    def apply_database_format_all(self) -> None:
-        """
-        Applies the standardized database-like format to all worksheets in the workbook:
+        # Hide gridlines
+        worksheet.hide_gridlines(2)
 
-        - Sets column widths and hides gridlines.
-        - Applies number formatting to numeric cells (with optional decimal places).
-        - Formats the first column (assumed to contain years or dates) with a light gray background.
-        - Formats the first row (assumed to contain headers) with a dark blue background, bold white text, and centered alignment.
-        - Applies light border to data containing cells.
-        
-        Returns
-        -------
-        None
-        """
-        for index in range(len(self.wb.worksheets)): 
-            self.apply_database_format(index)
+        # Determine format for the first column and adjust for datetime
+        first_col = df.columns[0]
+        first_col_fmt = self.workbook.add_format(self.format_cells['first_column'])
+        if pd.api.types.is_datetime64_any_dtype(df[first_col]):
+            first_col_fmt.set_num_format('mmm-yy')
 
+        # Write headers with header format
+        for col_num, col_name in enumerate(df.columns):
+            worksheet.write(0, col_num, col_name, self.workbook.add_format(self.format_cells["header"]))
 
-    def save_workbook(self, output_name: str)-> None:
-        """Save your workbook. Automatically includes extension in the name if not declared.
+        # Define format for numeric data columns once
+        fmt = self.workbook.add_format(self.format_cells['data'])
+        fmt.set_num_format(num_format)
 
-        Args:
-            name (str, optional): Choose a name for your Excel file. Defaults to "excel_test".
-        """
-        
-        output_path = os.path.join(save_dir, f'{output_name}.xlsx') if output_name else self.file_path
-        self.wb.save(output_path)
-        print(f'✅ Excel guardado como "{output_name}"')
+        # Write data cells with appropriate formats
+        for row_idx in range(df.shape[0]):
+            # First column (e.g., dates or text)
+            cell_value = df.iloc[row_idx, 0]
+            worksheet.write(row_idx + 1, 0, cell_value, first_col_fmt)
+
+            # Other columns (numeric data)
+            for col_idx in range(1, df.shape[1]):
+                cell_value = df.iloc[row_idx, col_idx]
+
+                # Skip NaN/Inf values by checking if the value is NaN or Inf
+                if pd.isna(cell_value) or np.isinf(cell_value):
+                    worksheet.write(row_idx + 1, col_idx, '')  # Write an empty cell
+                else:
+                    worksheet.write(row_idx + 1, col_idx, cell_value, fmt)
+    
+
