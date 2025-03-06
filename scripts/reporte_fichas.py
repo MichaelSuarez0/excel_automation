31edb5b0269e880015d7bee8d73d65def54350bb
MichaelSuarez0 <a20180264@pucp.edu.pe>
2025-03-05 22:15:40 -0500

    Update reporte_fichas.py

--- scripts/reporte_fichas.py
+++ scripts/reporte_fichas.py
@@ -41,30 +41,31 @@ def create_report(file_list: list) -> None:
     excel_app.save_new_workbook("Reporte Lima")
 
 # TODO: Set zoom 90% for all sheets after copying
-def create_carlomar_report(file_list: list) -> None:
-    table = defaultdict(list)
-    for file_full_name in file_list[:2]:
-        number = extract_number(file_full_name)
-        file_name = extract_file_name(file_full_name)
-        table["N°"].append(number)
-        table["Oportunidad"].append(file_name)
+# def create_carlomar_report(file_list: list) -> None:
+#     table = defaultdict(list)
+#     for file_full_name in file_list[:2]:
+#         number = extract_number(file_full_name)
+#         file_name = extract_file_name(file_full_name)
+#         table["N°"].append(number)
+#         table["Oportunidad"].append(file_name)
 
-        excel_app.read_workbook(file_full_name)
-        excel_app.rename_sheets()
-        excel_app.add_rows_to_all_sheets(3)
-        excel_app.write_to_cell_all_sheets(1, 1, f"Oportunidad {number}. {file_name}")
-        excel_app.freeze_top_row_all_sheets()
+#         excel_app.read_workbook(file_full_name)
+#         excel_app.rename_sheets()
+#         excel_app.add_rows_to_all_sheets(3)
+#         excel_app.write_to_cell_all_sheets(1, 1, f"Oportunidad {number}. {file_name}")
+#         excel_app.freeze_top_row_all_sheets()
 
-        excel_app.copy_sheets()
-        excel_app.close_workbook()
-    df = pd.DataFrame(table)
-    excel_app.write_table("0", df)
-    excel_app.save_new_workbook("Reporte Lima - Carlomar")
+#         excel_app.copy_sheets()
+#         excel_app.close_workbook()
+#     df = pd.DataFrame(table)
+#     excel_app.write_table("0", df)
+#     excel_app.save_new_workbook("Reporte Lima - Carlomar")
 
-
-def create_report_test(file_list: list, df: pd.DataFrame) -> None:
+# TODO: Casillas de Fuente 1, Fuente 2
+# TODO: Mover fuente dependiendo de la longitud del enlace
+def create_carlomar_report(file_list: list[str], df_list: list[pd.DataFrame]) -> None:
     # Asume que la primera hoja tiene el índice de las figuras
-    for file_full_name in file_list:
+    for file_full_name, df in zip(file_list, df_list):
         number = extract_number(file_full_name)
         file_name = extract_file_name(file_full_name)
 
@@ -74,18 +75,17 @@ def create_report_test(file_list: list, df: pd.DataFrame) -> None:
         excel_app.add_rows_to_all_sheets(5)
         excel_app.freeze_top_row_all_sheets()
 
-        ic(df)
         for sheet in range(excel_app.count_sheets):
             last_row = excel_app.get_last_row(sheet+1)
             excel_app.write_to_cell(sheet+1, 3, 1, df.iloc[sheet,1], bold=True)
             excel_app.write_to_cell(sheet+1, 4, 1, df.iloc[sheet,3])
             if df.iloc[sheet,2]:
-                if last_row < 30:
-                    excel_app.write_to_cell(sheet+1, last_row+3, 1, "Fuente:", bold=True)
-                    excel_app.write_to_cell(sheet+1, last_row+3, 2, df.iloc[sheet,2])
+                if last_row < 32:
+                    excel_app.write_to_cell(sheet+1, last_row+4, 1, "Fuente:", bold=True)
+                    excel_app.write_to_cell(sheet+1, last_row+4, 2, df.iloc[sheet,2])
                 else:
-                    excel_app.write_to_cell(sheet+1, 25, 7, "Fuente:", bold=True)
-                    excel_app.write_to_cell(sheet+1, 25, 8, df.iloc[sheet,2])
+                    excel_app.write_to_cell(sheet+1, 3, 8, "Fuente:", bold=True)
+                    excel_app.write_to_cell(sheet+1, 4, 8, df.iloc[sheet,2])
 
         excel_app.add_columns_to_all_sheets(1, 2)
         excel_app.write_to_cell_all_sheets(1, 1, f"Oportunidad {number}. {file_name}")
@@ -102,19 +102,26 @@ def create_report_test(file_list: list, df: pd.DataFrame) -> None:
     
     #excel_app.close()
 
-
 if __name__ == "__main__":
     # Initialize the ExcelCompiler and FileManager classes
-    # excel_app = ExcelCompiler(open_new=False)
-    # file_manager = FileManager(excel_app.reading_path)
+    excel_app = ExcelCompiler(open_new=True)
+    file_manager = FileManager(excel_app.reading_path)
+    custom_path = os.path.join("products", "oportunidades")
 
-    # # Obtain file_list
-    # file_list = file_manager.list_files(with_extension=False)
-    # file_list = FileManager.sort_files_by_number(file_list)
+    # Obtain file_list
+    file_list = file_manager.list_files(with_extension=False)
+    file_list = FileManager.sort_files_by_number(file_list)
+    index_list = []
+
+    # Obtain index_list
+    for file_name in file_list:
+        excel_extractor = ExcelDataExtractor(file_name, custom_path=custom_path)
+        df = excel_extractor.worksheet_to_dataframe(0)
+        index_list.append(df)
+
+    create_carlomar_report(file_list, index_list)
+
 
     # #create_report(file_list)
     # create_carlomar_report(file_list)
-    excel_extractor = ExcelDataExtractor("o2_lim - Aprovechamiento de la franja y ruta de la seda", custom_path=os.path.join("products", "oportunidades"))
-    df = excel_extractor.worksheet_to_dataframe(0)
-    excel_app = ExcelCompiler(open_new=False)
-    create_report_test(["o2_lim - Aprovechamiento de la franja y ruta de la seda"], df)
+    
