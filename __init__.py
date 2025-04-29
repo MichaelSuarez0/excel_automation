4a25a7dd2555d14392ef6e1f196071568692e39a
MichaelSuarez0 <a20180264@pucp.edu.pe>
2025-04-29 11:30:26 -0500

    Setup enhancements

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .excel_automation.core import ExcelAutoChart, ExcelDataExtractor
+from .excel_automation.utils import Color, Formats
+
+__all__ = [
+    "ExcelAutoChart",
+    "ExcelDataExtractor",
+    "Color",
+    "Formats"]
